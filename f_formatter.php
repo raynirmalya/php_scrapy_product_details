746fe75399a19e076fa69dc8bf8f8226cac6c860
raynirmalya <nirmalya.roy.21@gmail.com>
2016-06-07 06:36:41 +0530

    Revert "Deals Data"
    This reverts commit 6742925ca15e3349a02df887a37df779b5b4867d.

--- f_formatter.php
+++
@@ -1,5 +0,0 @@
-<?php
-function cashOnDeliveryBeautify($str){
-	echo $str;
-	return str_replace("Cash On Delivery ?", "",str_replace("Delivered By ?", "", $str));
-}
--- s_format.php
+++
@@ -1,7 +0,0 @@
-<?php
-ini_set('max_execution_time', 10000000000);
-include_once 'com/formatter/TitleFormatter.php';
-
-$titleFormatter=new TitleFormatter();
-$titleFormatter->formatTitleOfFProducts();
-?>
